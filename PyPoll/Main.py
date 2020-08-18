cfabdb2df28a27f510fff7e7c928537b7d576467
AGGII <aggoins2@gmail.com>
2020-08-17 21:56:57 -0500

    Importing modules and listing file-path

---
+++ PyPoll/Main.py
@@ -0,0 +1,5 @@
+#import necessary modules
+import os
+import csv
+## list relative path - directions to file from current file location
+file_path = os.path.join("..", "Resources", "election_data.csv")
