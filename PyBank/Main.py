8c60cbb367c62873f53b5b93710b472ab01cfa32
AGGII <aggoins2@gmail.com>
2020-08-17 22:19:39 -0500

    Read file, preserved list structure, stored data.

--- PyBank/Main.py
+++ PyBank/Main.py
@@ -1,8 +1,16 @@
-#In this challenge, you are tasked with creating a Python script for analyzing the financial records of your company. You will give a set of financial data called budget_data.csv. The dataset is composed of two columns: Date and Profit/Losses. (Thankfully, your company has rather lax standards for accounting so the records are simple.)
-#Your task is to create a Python script that analyzes the records to calculate each of the following:
-
 #import necessary modules
 import os
 import csv
 ## list relative path - directions to file from current file location
 file_path = os.path.join("..", "Resources", "budget_data.csv")
+##read into file, or write into file
+with open(file_path, "r") as csvfile:
+ # preserve csv structure
+    csvreader = csv.reader(csvfile, delimiter = ",")
+    #preview data - skip header
+    header = next(csvreader)
+    #store list as variable
+    contents = []
+
+    for row in csvreader:
+        contents.append(row)
--- PyPoll/Main.py
+++ PyPoll/Main.py
@@ -3,3 +3,14 @@ import os
 import csv
 ## list relative path - directions to file from current file location
 file_path = os.path.join("..", "Resources", "election_data.csv")
+##read into file, or write into file
+with open(file_path, "r") as csvfile:
+ # preserve csv structure
+    csvreader = csv.reader(csvfile, delimiter = ",")
+    #preview data - skip header
+    header = next(csvreader)
+    #store list as variable
+    contents = []
+
+    for row in csvreader:
+        contents.append(row)
