f13926beb7a5a7420a9a12f2bef6729a861209cf
AGGII <aggoins2@gmail.com>
2020-08-20 18:11:37 -0500

    Debug Financial Analysis formatting, month count and file path

--- PyBank/Main.py
+++ PyBank/Main.py
@@ -3,16 +3,18 @@ import os
 import csv
 
 # list relative path
-input_path = os.path.join( "Resources", "budget_data.csv")
-output_path = os.path.join( "Analysis", "Results.txt")
+in_path = os.path.join("Resources", "budget_data.csv")
+out_path = os.path.join( "Analysis", "Results.txt")
+
 #set variables
 Total_Months = 0
 Net_Total_PL = 0
 Avg_Change = 0
 Greatest_Increase_PL = 0
 Greatest_Decrease_PL = 0
+
 #read into file
-with open(input_path, "r") as csvfile:
+with open(in_path, "r") as csvfile:
     
     # preserve csv structure
     csvreader = csv.reader(csvfile, delimiter = ",")
@@ -23,18 +25,18 @@ with open(input_path, "r") as csvfile:
 
     #analyze data
     for row in csvreader:
-        Total_Months + 1
+        Total_Months += 1
         #contents.append(row[0])
 
 #write into file
-with open(output_path, "w") as txtfile:
-    txtfile.writer("Financial Analysis")
-    txtfile.writer("---------------------------")
-    txtfile.writer(f"Total Months: {Total_Months}")
-    txtfile.writer("Net Total-Profits/Losses")
-    txtfile.writer("Avg Change-Profits/Losses")
-    txtfile.writer("Greatest Increase-Profits")
-    txtfile.writer("Greatest Decrease-Profits")
+with open(out_path, "w") as txtfile:
+    txtfile.write("Financial Analysis\n")
+    txtfile.write("----------------------------------------\n")
+    txtfile.write(f"Total Months: {Total_Months}\n")
+    txtfile.write("Net Total-Profits/Losses:\n")
+    txtfile.write("Avg Change-Profits/Losses:\n")
+    txtfile.write("Greatest Increase-Profits:\n")
+    txtfile.write("Greatest Decrease-Profits:\n")
     
-print(contents)
+#print(Analysis)
 
